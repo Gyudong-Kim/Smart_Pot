4297ec9194fcb3088b8e6c5560ba0d8176dec932
Gyudong-Kim <dong3955@gmail.com>
2022-11-16 17:21:56 +0900

    Info 페이지 구현

--- lib/views/control_view.dart
+++ lib/views/control_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class status extends StatelessWidget {
+class ControlPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
       child: Center(
-        child: Text("상태 페이지"),
+        child: Text("제어 페이지"),
       ),
     ));
   }
